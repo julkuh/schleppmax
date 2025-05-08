878e925a52ebca838f4f531e2b6fec30d8e7b60f
Julian Kuhlmann <j.kuhlmann@conterra.de>
2025-05-08 21:37:27 +0200

    small fix

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
 
 <script lang="ts" setup>
   import { onMounted } from 'vue';
-  import { positions, calculateVerticalSpeed } from '@/composables/usePositions';
+  import { calculateVerticalSpeed, positions } from '@/composables/usePositions';
 
   onMounted(() => {
     if (navigator.geolocation) {
